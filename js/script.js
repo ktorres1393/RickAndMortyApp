1494f1afc1a9b5b98b97c7179340776fae0d6cc7
Kassandra Torres <ktorres1393@gmail.com>
2020-10-29 10:28:16 -0400

    adds Hero Image

--- js/script.js
+++ js/script.js
@@ -5,22 +5,49 @@
 /*----- event listeners -----*/
 /*----- functions -----*/
 
-const BASE_URL = 'https://rickandmortyapi.com/api/'
+    // listen to events on button
+    $('#character-btn').on('click', function (event) {
+        // prevent page from refreshing 
+        event.preventDefault();
 
-const response = $.ajax({
-    url: 'https://rickandmortyapi.com/api/character/183'
-})
+        //get name in input field
+        const name =  $('#character-name').val();
+        console.log(name);
+    
+        // url request to get info based on name
+        const response = $.ajax({
+            url: 'https://rickandmortyapi.com/api/character/?name='+name
+        })
+        
+        response.then(
+            (data) => {
+                console.log(data.results);
 
-response.then(
-    (data) => {
-        console.log(data);
-        $('#character').text(data.name)
-        $('#alive').text(data.status)
-        $('#species').text(data.species)
-        $('#planet').text(data.origin)
-        $('#character-img').attr("src", data.image)
-    },
-    (error) => {
-        console.log("bad request: ", error)
-    }
-)
+                let html = "";
+
+                data.results.forEach(element => {
+                    let card = `<div class="card" style="width: 18rem;">
+                    <img src="${element.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Characters Name</h5>
+                    <p id="character">Name: ${element.name}</p>
+                    <p id="alive">Character Status: ${element.status}</p>
+                    <p id="species">Species: ${element.species}</p>
+                    <p id="planet">Birth Planet: ${element.origin.name}</p>
+                    </div>
+                  </div>` 
+                    
+                 html =  html.concat(card);
+                });
+
+                $( "main" ).append( html );
+            },
+            (error) => {
+
+                //---------------------------------------------//
+                //              handle error
+                //---------------------------------------------//
+                console.log("bad request: ", error)
+            }
+        )
+    });
